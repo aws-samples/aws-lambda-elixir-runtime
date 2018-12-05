afe4d068b32f078d77dbf441387149e00ca552ad
brlyman <brlyman@amazon.com>
2018-12-05 11:10:25 -0800

    Add Copyright Headers To Example Files

--- examples/hello_world/lib/hello_world.ex
+++ examples/hello_world/lib/hello_world.ex
@@ -1,3 +1,6 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 defmodule HelloWorld do
   @moduledoc """
   Entrypoint for my hello world Lambda function.
--- examples/hello_world/mix.exs
+++ examples/hello_world/mix.exs
@@ -1,3 +1,6 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.ja
+# SPDX-License-Identifier: MIT-0
+
 defmodule HelloWorld.MixProject do
   use Mix.Project
 
